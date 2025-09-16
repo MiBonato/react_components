5a7fe0f4a26e8b478aabc49510eb398310f8faf2
Michael Bonato <michael.bonato@gmail.com>
2025-09-16 23:46:33 +0200

    add form file

---
+++ src/components/form/Form.jsx
@@ -0,0 +1,7 @@
+export default function Form() {
+  return (
+    <>
+      <div>Form component placeholder</div>
+    </>
+  )
+}
--- src/content/FormField.jsx
+++ src/content/FormField.jsx
@@ -1,12 +1,9 @@
-import Form from '../components/Form' 
+import Form from '../components/form/Form' 
 
 export default function Formfield() {
   return (
-    <div className="mx-auto max-w-6xl px-4 py-8 space-y-4">
-      <h1 className="text-2xl font-semibold">Form</h1>
-      <div className="flex flex-wrap gap-3">
-        <Form />
-      </div>
-    </div>
+    <>
+      <Form />
+    </>
   )
 }
